e40f4a63dd6fb35c62bd1c37341b19af702904f1
Kelvin Purdom <kelvin.purdom@gmail.com>
2022-05-24 15:43:15 +0200

    Corrected code for rsme

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,3 +1,5 @@
+from unittest import result
+import mlflow
 from sklearn.linear_model import LinearRegression
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
@@ -5,6 +7,13 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
 from TaxiFareModel.utils import compute_rmse
+from memoized_property import memoized_property
+from mlflow.tracking import MlflowClient
+from TaxiFareModel.data import get_data, clean_data
+from sklearn.model_selection import train_test_split
+
+EXPERIMENT_NAME = "[DE] [Berlin] [kelvinpurdom] TaxiFareModel version 2"
+MLFLOW_URI = "https://mlflow.lewagon.ai/"
 
 class Trainer():
     def __init__(self, X, y):
@@ -15,6 +24,7 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = EXPERIMENT_NAME
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -48,15 +58,49 @@ class Trainer():
 
         y_pred = self.pipe.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
-        print(rmse)
+        self.mlflow_log_metric('rmse', rmse)
         return rmse
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
     # get data
+    df = get_data()
     # clean data
+    df = clean_data(df)
     # set X and y
+    X = df.drop(columns= 'fare_amount')
+    y = df['fare_amount']
     # hold out
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
     # train
+    trainer = Trainer(X_train,y_train)
+    trainer.run()
     # evaluate
-    print('TODO')
+    results = trainer.evaluate(X_test, y_test)
+    print(results)
+    experiment_id = trainer.mlflow_experiment_id
+
+    print(f"experiment URL: https://mlflow.lewagon.ai/#/experiments/{experiment_id}")
+    print(trainer.experiment_name)
